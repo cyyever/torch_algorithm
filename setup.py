11e285ced23d878c90f49bc7d4e82e28f6b713c9
cyy <cyyever@outlook.com>
2021-06-07 21:53:28 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cyy_torch_algorithm",
+    author="cyy",
+    version="0.1",
+    author_email="cyyever@outlook.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cyyever/torch_algorithm",
+    packages=[
+        "cyy_torch_algorithm",
+        "cyy_torch_algorithm/hydra",
+        "cyy_torch_algorithm/video",
+        "cyy_torch_algorithm/sample_gradient",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
