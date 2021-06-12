0bc0b79d595f468aefd211f39f4a749e04a07d5c
cyy <cyyever@outlook.com>
2021-06-12 15:54:15 +0800

    fix code

--- cyy_torch_algorithm/quantization/scheme.py
+++ cyy_torch_algorithm/quantization/scheme.py
@@ -9,12 +9,12 @@ from cyy_torch_toolbox.tensor import (cat_tensors_to_vector,
                                       split_tensor_to_dict)
 
 
-def stochastic_quantization(
-    quantization_level: int, use_l2_norm: bool = False
-) -> Tuple[Callable, Callable]:
-    """Implement Stochastic Quantization as described in QSGD: Communication-Efficient SGDvia Gradient Quantization and Encoding (https://arxiv.org/pdf/1610.02132.pdf)"""
+class StocasticQuant:
+    def __init__(self, quantization_level, use_l2_norm):
+        self.quantization_level = quantization_level
+        self.use_l2_norm = use_l2_norm
 
-    def quant(tensor):
+    def __call__(self, tensor):
         name_and_shapes = None
         if isinstance(tensor, dict):
             name_and_shapes = []
@@ -34,7 +34,7 @@ def stochastic_quantization(
             assert len(tensor.shape) == 1
 
             norm = None
-            if use_l2_norm:
+            if self.use_l2_norm:
                 norm = torch.linalg.norm(tensor)
             else:
                 norm = torch.linalg.norm(tensor, ord=float("inf"))
@@ -42,9 +42,9 @@ def stochastic_quantization(
             sign_tensor = torch.sign(tensor)
             normalized_abs_tensor = tensor.abs() / norm
             slot_tensor = cyy_naive_cpp_extension.torch.stochastic_quantization(
-                normalized_abs_tensor, quantization_level
+                normalized_abs_tensor, self.quantization_level
             )
-            prob_tensor = normalized_abs_tensor * quantization_level - slot_tensor
+            prob_tensor = normalized_abs_tensor * self.quantization_level - slot_tensor
             random_vector = torch.distributions.Bernoulli(prob_tensor).sample()
             slot_tensor += random_vector
 
@@ -56,9 +56,17 @@ def stochastic_quantization(
                 old_tensor_shape
             )
 
-            return (norm, sign_tensor, slot_tensor, quantization_level, name_and_shapes)
+            return (
+                norm,
+                sign_tensor,
+                slot_tensor,
+                self.quantization_level,
+                name_and_shapes,
+            )
 
-    def dequant(quantized_pair):
+
+class StocasticDequant:
+    def __call__(self, quantized_pair):
         (
             norm,
             sign_tensor,
@@ -75,4 +83,9 @@ def stochastic_quantization(
             res = split_tensor_to_dict(name_and_shapes, res)
         return res
 
-    return (quant, dequant)
+
+def stochastic_quantization(
+    quantization_level: int, use_l2_norm: bool = False
+) -> Tuple[Callable, Callable]:
+    """Implement Stochastic Quantization as described in QSGD: Communication-Efficient SGDvia Gradient Quantization and Encoding (https://arxiv.org/pdf/1610.02132.pdf)"""
+    return StocasticQuant(quantization_level, use_l2_norm), StocasticDequant()
