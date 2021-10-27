cb48196d39b737993c1723dfcc0de6cd030850f9
cyy <cyyever@outlook.com>
2021-10-27 10:39:21 +0700

    add code

--- cyy_torch_algorithm/hessian_vector_product.py
+++ cyy_torch_algorithm/hessian_vector_product.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 
 import atexit
+import collections
 
 import torch
-import torch.autograd as autograd
 from cyy_naive_lib.algorithm.sequence_op import split_list_to_chunks
-from cyy_torch_toolbox.data_structure.torch_process_task_queue import TorchProcessTaskQueue
+from cyy_torch_toolbox.data_structure.torch_process_task_queue import \
+    TorchProcessTaskQueue
 from cyy_torch_toolbox.device import get_devices
 from cyy_torch_toolbox.model_util import ModelUtil
 from cyy_torch_toolbox.model_with_loss import ModelWithLoss
+from torch import autograd
 
 
 def __get_f(device, inputs, targets, model_with_loss: ModelWithLoss):
@@ -57,7 +59,6 @@ __task_queue = None
 
 
 def stop_task_queue():
-    global __task_queue
     if __task_queue is not None:
         __task_queue.force_stop()
 
@@ -65,7 +66,9 @@ def stop_task_queue():
 atexit.register(stop_task_queue)
 
 
-def get_hessian_vector_product_func(model_with_loss: ModelWithLoss, batch):
+def get_hessian_vector_product_func(
+    model_with_loss: ModelWithLoss, batch, main_device=None
+):
     devices = get_devices()
 
     model = model_with_loss.model
@@ -79,16 +82,15 @@ def get_hessian_vector_product_func(model_with_loss: ModelWithLoss, batch):
 
     def vhp_func(v):
         global __task_queue
+        nonlocal main_device
         v_is_tensor = False
-        if isinstance(v, list):
+        if isinstance(v, collections.abc.Sequence):
             vectors = v
-        elif isinstance(v, tuple):
-            vectors = list(v)
         else:
             v_is_tensor = True
             vectors = [v]
 
-        vector_chunks = list(
+        vector_chunks = tuple(
             split_list_to_chunks(
                 vectors, (len(vectors) + len(devices) - 1) // len(devices)
             )
@@ -97,13 +99,13 @@ def get_hessian_vector_product_func(model_with_loss: ModelWithLoss, batch):
 
         if __task_queue is None:
             __task_queue = TorchProcessTaskQueue(worker_fun)
-        __task_queue.start()
+            __task_queue.start()
         for idx, vector_chunk in enumerate(vector_chunks):
             __task_queue.add_task(
                 (idx, vector_chunk, model_with_loss, parameter_list, batch[0], batch[1])
             )
 
-        total_products = dict()
+        total_products = {}
         for _ in range(len(vector_chunks)):
             idx, gradient_list = __task_queue.get_result()
             total_products[idx] = gradient_list
@@ -112,8 +114,11 @@ def get_hessian_vector_product_func(model_with_loss: ModelWithLoss, batch):
         for idx in sorted(total_products.keys()):
             products += total_products[idx]
         assert len(products) == len(vectors)
+        if main_device is None:
+            main_device = devices[0]
+        products = [p.to(main_device) for p in products]
         if v_is_tensor:
             return products[0]
-        return [p.to(devices[0]) for p in products]
+        return products
 
     return vhp_func
