dadae0cf424804584b0cbf946f777e15795ce1d6
cyy <cyyever@outlook.com>
2022-02-09 01:24:08 +0800

    add code

--- cyy_torch_algorithm/sample_gradient/sample_gradient.py
+++ cyy_torch_algorithm/sample_gradient/sample_gradient.py
@@ -6,16 +6,19 @@ import torch
 from cyy_torch_toolbox.ml_type import MachineLearningPhase
 from cyy_torch_toolbox.model_util import ModelUtil
 
+local_data = threading.local()
+
 
 def sample_gradient_worker_fun(task, args):
-    worker_device = getattr(threading.local(), "worker_device", None)
-    worker_stream = None
+    global local_data
+
+    worker_device = getattr(local_data, "worker_device", None)
     if worker_device is None:
         worker_device = args["device"]
-        threading.local().worker_device = worker_device
+        local_data.worker_device = worker_device
         if worker_device.index is not None:
-            worker_stream = torch.cuda.Stream(device=worker_device)
-        threading.local().worker_stream = worker_stream
+            local_data.worker_stream = torch.cuda.Stream(device=worker_device)
+    worker_stream = getattr(local_data, "worker_stream", None)
     (index, input_chunk, target_chunk, model_with_loss) = task
     gradient_lists = []
     with torch.cuda.stream(worker_stream):
@@ -31,6 +34,7 @@ def sample_gradient_worker_fun(task, args):
                 sample_target,
                 phase=MachineLearningPhase.Test,
                 device=worker_device,
+                non_blocking=True,
             )["loss"]
             loss.backward()
             gradient_lists.append(model_util.get_gradient_list())
