1b273b0e7f05830b5f587bcebd417f59525188b6
cyy <cyyever@outlook.com>
2022-03-20 15:15:07 +0800

    fix code

--- cyy_torch_algorithm/hessian_vector_product.py
+++ cyy_torch_algorithm/hessian_vector_product.py
@@ -120,7 +120,7 @@ def get_hessian_vector_product_func(
 
         if __task_queue is None:
             if len(devices) > 0:
-                __task_queue = TorchProcessTaskQueue(worker_fun, move_data_in_cpu=False)
+                __task_queue = TorchProcessTaskQueue(worker_fun, move_data_in_cpu=True)
             else:
                 __task_queue = TorchThreadTaskQueue(worker_fun)
             __task_queue.start()
