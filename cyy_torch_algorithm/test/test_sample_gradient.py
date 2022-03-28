b2c987ec85323ad6f37a6e85ccc12dbfcf708d21
cyy <cyyever@outlook.com>
2022-03-28 20:07:21 +0800

    fix code

--- cyy_torch_algorithm/test/test_sample_gradient.py
+++ cyy_torch_algorithm/test/test_sample_gradient.py
@@ -9,6 +9,8 @@ from cyy_torch_toolbox.ml_type import (ModelExecutorHookPoint,
                                        StopExecutingException)
 from cyy_torch_toolbox.reproducible_env import global_reproducible_env
 
+global_reproducible_env.enable()
+
 
 def test_get_sample_gradient():
     config = DefaultConfig("MNIST", "lenet5")
@@ -33,23 +35,23 @@ def test_get_sample_gradient():
     )
     trainer.train()
 
-    global_reproducible_env.disable()
-    global_reproducible_env.load_last_seed()
-    global_reproducible_env.enable()
-    os.environ["reseed_dropout"] = "1"
+    # global_reproducible_env.disable()
+    # global_reproducible_env.load_last_seed()
+    # global_reproducible_env.enable()
+    # os.environ["reseed_dropout"] = "1"
 
-    config = DefaultConfig("MNIST", "lenet5")
-    config.hyper_parameter_config.epoch = 1
-    config.hyper_parameter_config.batch_size = 8
-    config.hyper_parameter_config.learning_rate = 0.01
-    config.hyper_parameter_config.find_learning_rate = False
-    trainer = config.create_trainer()
-    hook = SampleGradientHook()
-    hook.use_new = False
-    hook.set_computed_indices([1, 2, 3])
-    trainer.append_hook(hook)
+    # config = DefaultConfig("MNIST", "lenet5")
+    # config.hyper_parameter_config.epoch = 1
+    # config.hyper_parameter_config.batch_size = 8
+    # config.hyper_parameter_config.learning_rate = 0.01
+    # config.hyper_parameter_config.find_learning_rate = False
+    # trainer = config.create_trainer()
+    # hook = SampleGradientHook()
+    # hook.use_new = False
+    # hook.set_computed_indices([1])
+    # trainer.append_hook(hook)
 
-    trainer.append_named_hook(
-        ModelExecutorHookPoint.AFTER_BATCH, "check gradients", print_sample_gradients
-    )
-    trainer.train()
+    # trainer.append_named_hook(
+    #     ModelExecutorHookPoint.AFTER_BATCH, "check gradients", print_sample_gradients
+    # )
+    # trainer.train()
