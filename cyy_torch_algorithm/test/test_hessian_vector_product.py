c5f6603421bee224979994a24c7fcf84e96c2c8d
cyy <cyyever@outlook.com>
2022-03-29 11:26:37 +0800

    fix test

--- cyy_torch_algorithm/test/test_hessian_vector_product.py
+++ cyy_torch_algorithm/test/test_hessian_vector_product.py
@@ -1,9 +1,9 @@
 import torch
+from cyy_naive_lib.time_counter import TimeCounter
 from cyy_torch_toolbox.default_config import DefaultConfig
 from hessian_vector_product import (get_hessian_vector_product_func,
                                     stop_task_queue)
 
-# from cyy_naive_lib.time_counter import TimeCounter
 # from cyy_naive_lib.profiling import Profile
 
 
@@ -16,7 +16,6 @@ def test_hessian_vector_product():
         shuffle=False,
     )
     parameter_vector = trainer.model_util.get_parameter_list()
-    # parameter_vector.fill_(1)
     trainer.model_util.load_parameter_list(parameter_vector)
     v = torch.ones_like(parameter_vector)
     for batch in training_data_loader:
@@ -28,48 +27,49 @@ def test_hessian_vector_product():
         vector = [v * (i + 1) for i in range(11)]
         a = hvp_function(vector)
         assert len(a) == 11
-        assert torch.linalg.norm(a[1] - 2 * a[0], ord=2).data.item() < 0.5
-        assert torch.linalg.norm(a[2] - 3 * a[0], ord=2).data.item() < 0.5
-        assert torch.linalg.norm(a[10] - 11 * a[0], ord=2).data.item() < 0.5
+        print(a)
+        assert torch.linalg.norm(a[1] - 2 * a[0], ord=2).data.item() < 0.05
+        assert torch.linalg.norm(a[2] - 3 * a[0], ord=2).data.item() < 0.05
+        assert torch.linalg.norm(a[10] - 11 * a[0], ord=2).data.item() < 0.05
         del a
 
-        # with TimeCounter() as c:
-        #     a = hvp_function(v)
-        #     print("one use time ", c.elapsed_milliseconds())
-        #     print(a)
-        #     c.reset_start_time()
-        #     a = hvp_function([v, 2 * v])
-        #     print("two use time ", c.elapsed_milliseconds())
-        #     print(a)
-        #     c.reset_start_time()
-        #     a = hvp_function([v, 2 * v, 3 * v])
-        #     print("3 use time ", c.elapsed_milliseconds())
-        #     print(a)
-        #     c.reset_start_time()
-        #     a = hvp_function([v] * 4)
-        #     print("4 use time ", c.elapsed_milliseconds())
-        #     c.reset_start_time()
-        #     a = hvp_function([v] * 4)
-        #     print("4 use time ", c.elapsed_milliseconds())
-        #     c.reset_start_time()
-        #     a = hvp_function([v] * 64)
-        #     print("64 use time ", c.elapsed_milliseconds())
-        #     c.reset_start_time()
-        #     a = hvp_function([v] * 64)
-        #     print("64 use time ", c.elapsed_milliseconds())
-        #     c.reset_start_time()
-        #     a = hvp_function([v] * 64)
-        #     print("64 use time ", c.elapsed_milliseconds())
-        #     c.reset_start_time()
-        #     a = hvp_function([v] * 100)
-        #     print("100 use time ", c.elapsed_milliseconds())
-        #     c.reset_start_time()
-        #     a = hvp_function([v] * 100)
-        #     print("100 use time ", c.elapsed_milliseconds())
-        #     # with Profile():
-        #     #     c.reset_start_time()
-        #     #     a = hvp_function([v] * 100)
-        #     #     print("100 use time ", c.elapsed_milliseconds())
-        # del a
+        with TimeCounter() as c:
+            a = hvp_function(v)
+            print("one use time ", c.elapsed_milliseconds())
+            print(a)
+            c.reset_start_time()
+            a = hvp_function([v, 2 * v])
+            print("two use time ", c.elapsed_milliseconds())
+            print(a)
+            c.reset_start_time()
+            a = hvp_function([v, 2 * v, 3 * v])
+            print("3 use time ", c.elapsed_milliseconds())
+            print(a)
+            c.reset_start_time()
+            a = hvp_function([v] * 4)
+            print("4 use time ", c.elapsed_milliseconds())
+            c.reset_start_time()
+            a = hvp_function([v] * 4)
+            print("4 use time ", c.elapsed_milliseconds())
+            c.reset_start_time()
+            a = hvp_function([v] * 64)
+            print("64 use time ", c.elapsed_milliseconds())
+            c.reset_start_time()
+            a = hvp_function([v] * 64)
+            print("64 use time ", c.elapsed_milliseconds())
+            c.reset_start_time()
+            a = hvp_function([v] * 64)
+            print("64 use time ", c.elapsed_milliseconds())
+            c.reset_start_time()
+            a = hvp_function([v] * 100)
+            print("100 use time ", c.elapsed_milliseconds())
+            c.reset_start_time()
+            a = hvp_function([v] * 100)
+            print("100 use time ", c.elapsed_milliseconds())
+            # with Profile():
+            #     c.reset_start_time()
+            #     a = hvp_function([v] * 100)
+            #     print("100 use time ", c.elapsed_milliseconds())
+            del a
         break
     stop_task_queue()
