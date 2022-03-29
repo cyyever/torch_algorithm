d90b4dcbe87bad9bfdeed843a0e5b35adfa2a66f
cyy <cyyever@outlook.com>
2022-03-29 14:45:07 +0800

    refactor code

---
+++ cyy_torch_algorithm/evaluation.py
@@ -0,0 +1,28 @@
+from cyy_torch_toolbox.ml_type import MachineLearningPhase
+
+
+def eval_model_by_parameter(
+    parameter_list,
+    inputs,
+    targets,
+    device,
+    model_with_loss,
+    phase=MachineLearningPhase.Training,
+):
+    match parameter_list:
+        case tuple() | list():
+            parameter_list = parameter_list[0]
+    parameter_list = parameter_list.to(device)
+    model_util = model_with_loss.model_util
+    model_util.load_parameter_list(
+        parameter_list,
+        check_parameter=False,
+        as_parameter=False,
+    )
+    return model_with_loss(
+        inputs,
+        targets,
+        device=device,
+        non_blocking=False,
+        phase=phase,
+    )["loss"]
