b909c3c800cdd75cac69aab3d7485203e95a4b33
cyy <cyyever@outlook.com>
2022-03-29 17:16:29 +0800

    fix code

--- cyy_torch_algorithm/test/test_sample_gradient.py
+++ cyy_torch_algorithm/test/test_sample_gradient.py
@@ -23,7 +23,7 @@ def test_get_sample_gradient():
     config.hyper_parameter_config.find_learning_rate = False
     trainer = config.create_trainer()
     hook = SampleGradientHook()
-    hook.set_computed_indices(set(range(100)))
+    hook.set_computed_indices(set(range(10)))
     trainer.append_hook(hook)
 
     def print_sample_gradients(**kwargs):
