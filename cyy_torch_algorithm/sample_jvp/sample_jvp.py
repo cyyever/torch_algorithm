0f9f19d0eb60c3222e323c25205b5170dfb5d38c
cyy <cyyever@outlook.com>
2022-04-13 22:14:48 +0800

    fix code

--- cyy_torch_algorithm/sample_jvp/sample_jvp.py
+++ cyy_torch_algorithm/sample_jvp/sample_jvp.py
@@ -28,7 +28,6 @@ def sample_jvp_worker_fun(task, args):
         sample_indices, input_chunk, target_chunk, vector_chunk
     ):
         input_shape = input_tensor.shape
-        print("input_tensor is",input_tensor)
 
         def grad_f(input_tensor):
             return grad(f, argnums=0)(
