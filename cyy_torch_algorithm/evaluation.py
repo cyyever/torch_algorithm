a2d77cdfe747487165682697142ded6fbe12e5ba
cyy <cyyever@outlook.com>
2022-04-25 13:30:25 +0800

    fix code

--- cyy_torch_algorithm/evaluation.py
+++ cyy_torch_algorithm/evaluation.py
@@ -31,3 +31,45 @@ def eval_model_by_parameter(
         phase=phase,
         model_fun=model_fun,
     )["loss"]
+
+
+def eval_model_foreach(
+    parameter_list,
+    inputs,
+    targets,
+    device,
+    model_with_loss,
+    input_shape=None,
+    model_util=None,
+    phase=MachineLearningPhase.Training,
+    forward_embedding=False,
+):
+    if model_util is None:
+        model_util = model_with_loss.model_util
+    model_util.load_parameter_list(
+        parameter_list.to(device),
+        check_parameter=False,
+        as_parameter=False,
+    )
+    if not forward_embedding:
+        model_fun = model_with_loss.model
+    else:
+        model_fun = model_with_loss.model.forward_embedding
+
+    total_loss = None
+    for sample_input, sample_target in zip(inputs, targets):
+        if input_shape is not None:
+            sample_input = sample_input.view(input_shape)
+        loss = model_with_loss(
+            sample_input,
+            sample_target,
+            device=device,
+            non_blocking=False,
+            phase=phase,
+            model_fun=model_fun,
+        )["loss"]
+        if total_loss is None:
+            total_loss = loss
+        else:
+            total_loss += loss
+    return total_loss
--- cyy_torch_algorithm/sample_vjp/sample_vjp.py
+++ cyy_torch_algorithm/sample_vjp/sample_vjp.py
@@ -3,7 +3,7 @@ import functools
 import threading
 
 import torch.autograd
-from evaluation import eval_model_by_parameter
+from evaluation import eval_model_foreach
 from functorch import grad
 
 local_data = threading.local()
@@ -18,36 +18,35 @@ def sample_vjp_worker_fun(product_transform, vector, task, args):
     model_with_loss.model.to(worker_device)
     vector = vector.to(worker_device)
     forward_embedding = hasattr(model_with_loss.model, "forward_embedding")
+    raw_input_chunk = [t.to(worker_device) for t in input_chunk]
+    if forward_embedding:
+        input_chunk = [
+            model_with_loss.model.get_embedding(raw_input_tensor).detach()
+            for raw_input_tensor in raw_input_chunk
+        ]
     f = functools.partial(
-        eval_model_by_parameter,
+        eval_model_foreach,
+        targets=target_chunk,
         device=worker_device,
         model_with_loss=model_with_loss,
+        input_shape=input_chunk[0].shape,
         model_util=model_with_loss.model_util,
         forward_embedding=forward_embedding,
     )
+
     parameter_list = model_with_loss.model_util.get_parameter_list(detach=True)
+
+    def grad_f(*input_tensors):
+        return grad(f, argnums=0)(parameter_list, input_tensors).view(-1)
+
+    products = product = torch.autograd.functional.vjp(
+        func=grad_f, inputs=tuple(t.view(-1) for t in input_chunk), v=vector
+    )[1]
+
     result = {}
-    for index, raw_input_tensor, target in zip(
-        sample_indices, input_chunk, target_chunk
+    for index, raw_input_tensor, input_tensor, product in zip(
+        sample_indices, raw_input_chunk, input_chunk, products
     ):
-        raw_input_tensor = raw_input_tensor.to(worker_device)
-        if forward_embedding:
-            input_tensor = model_with_loss.model.get_embedding(
-                raw_input_tensor
-            ).detach()
-        else:
-            input_tensor = raw_input_tensor
-        input_shape = input_tensor.shape
-        input_tensor = input_tensor.view(-1)
-
-        def grad_f(input_tensor):
-            return grad(f, argnums=0)(
-                parameter_list, input_tensor.view(input_shape), target
-            ).view(-1)
-
-        product = torch.autograd.functional.vjp(
-            func=grad_f, inputs=input_tensor, v=vector
-        )[1]
         if product_transform is not None:
             product = product_transform(index, raw_input_tensor, input_tensor, product)
         result[index] = product
