bcf015193fad23e9cffe38d6b79413049e71a5a9
cyy <cyyever@outlook.com>
2022-06-21 16:54:08 +0800

    fix algorithm

--- cyy_torch_algorithm/quantization/stochastic.py
+++ cyy_torch_algorithm/quantization/stochastic.py
@@ -1,8 +1,10 @@
-from typing import Callable, Tuple
+import math
+from typing import Any, Callable, Tuple
 
 import numpy
 import torch
 from cyy_naive_lib.algorithm.mapping_op import get_mapping_values_by_key_order
+from cyy_naive_lib.log import get_logger
 from cyy_torch_toolbox.device import get_cpu_device
 from cyy_torch_toolbox.tensor import (cat_tensors_to_vector,
                                       split_tensor_to_dict)
@@ -86,3 +88,202 @@ def stochastic_quantization(
 ) -> Tuple[Callable, Callable]:
     """Implement Stochastic Quantization as described in QSGD: Communication-Efficient SGDvia Gradient Quantization and Encoding (https://arxiv.org/pdf/1610.02132.pdf)"""
     return StochasticQuant(quantization_level, use_l2_norm), StochasticDequant()
+
+
+class ImprovedStochasticQuant:
+    def __init__(
+        self, weight: float, use_l2_norm: bool = False, normalization: bool = True
+    ):
+        self.weight = weight
+        self.use_l2_norm = use_l2_norm
+        self.normalization = normalization
+
+    def __call__(self, tensor):
+        element_bits = None
+        dtype = tensor.dtype
+        element_bits = tensor.element_size() * 8
+        old_tensor_shape = tensor.shape
+        tensor = tensor.to(dtype=torch.float64)
+        tensor = tensor.view(-1)
+        if self.normalization:
+            mean = torch.mean(tensor)
+            tensor = tensor - mean
+        else:
+            mean = None
+
+        norm = None
+        if self.use_l2_norm:
+            norm = torch.linalg.norm(tensor)
+        else:
+            norm = torch.linalg.norm(tensor, ord=float("inf"))
+        if norm == 0.0:
+            return {
+                "dtype": dtype,
+                "mean": mean,
+                "tensor_shape": old_tensor_shape,
+                "compression_ratio": 0,
+            }
+
+        quantization_level = int(
+            max(1, math.sqrt(norm * element_bits * math.log(4) / self.weight))
+        )
+        compression_ratio = math.ceil(math.log2(quantization_level + 1)) / element_bits
+        sign_tensor = torch.sign(tensor)
+        normalized_abs_tensor = tensor.abs() / norm
+        tmp = normalized_abs_tensor * quantization_level
+        slot_tensor = tmp.trunc()
+        prob_tensor = tmp - slot_tensor
+        random_vector = torch.distributions.Bernoulli(prob_tensor).sample()
+        slot_tensor += random_vector
+
+        sign_tensor = numpy.packbits(
+            ((sign_tensor + 1) / 2).to(torch.bool).to(get_cpu_device()).numpy()
+        )
+        if quantization_level < 2**8:
+            new_dtype = numpy.uint8
+        elif quantization_level < 2**16:
+            new_dtype = numpy.uint16
+        elif quantization_level < 2**32:
+            new_dtype = numpy.uint32
+        else:
+            raise RuntimeError(f"invalid quantization level {quantization_level}")
+        slot_tensor = (
+            slot_tensor.reshape(old_tensor_shape).numpy().astype(dtype=new_dtype)
+        )
+        return {
+            "dtype": dtype,
+            "norm": norm,
+            "sign_tensor": sign_tensor,
+            "quantized_tensor": slot_tensor,
+            "quantization_level": quantization_level,
+            "mean": mean,
+            "compression_ratio": compression_ratio,
+        }
+
+
+class ImprovedStochasticDequant:
+    def __call__(self, quantized_dict: dict) -> torch.Tensor:
+        dtype = quantized_dict["dtype"]
+        mean = quantized_dict["mean"]
+        if "tensor_shape" in quantized_dict:
+            quantized_tensor = torch.zeros(
+                quantized_dict["tensor_shape"], dtype=torch.float64
+            )
+            if mean is not None:
+                quantized_tensor += mean
+            return quantized_tensor.to(dtype=dtype)
+
+        quantized_tensor = torch.from_numpy(
+            quantized_dict["quantized_tensor"].astype(dtype=numpy.int64)
+        ).to(dtype=torch.float64)
+        sign_tensor = quantized_dict["sign_tensor"]
+        quantization_level = quantized_dict["quantization_level"]
+        norm = quantized_dict["norm"]
+        quantized_tensor *= norm
+        sign_tensor = (torch.from_numpy(numpy.unpackbits(sign_tensor)).float() * 2 - 1)[
+            : numpy.prod(quantized_tensor.shape)
+        ].reshape(quantized_tensor.shape)
+        res = (
+            quantized_tensor
+            * sign_tensor.to(quantized_tensor.device)
+            / quantization_level
+        )
+        if mean is not None:
+            res = res + mean
+        return res.to(dtype=dtype)
+
+
+class NeuralNetworkImprovedStochasticQuant(ImprovedStochasticQuant):
+    def __call__(self, data: Any) -> Any:
+        match data:
+            case dict():
+                return {k: self.__call__(v) for k, v in data.items()}
+            case torch.Tensor():
+                return super().__call__(data)
+            case _:
+                return data
+
+    @classmethod
+    def check_compression_ratio(cls, quantized_data):
+        result = []
+        compressed_parameter_num = 0
+        total_parameter_num = 0
+        quantization_levels = []
+        parameter_numbers = []
+
+        def collection(quantized_data):
+            nonlocal result, total_parameter_num, quantization_levels, compressed_parameter_num
+            if not isinstance(quantized_data, dict):
+                return
+            if "tensor_shape" in quantized_data:
+                parameter_num = numpy.prod(quantized_data["tensor_shape"])
+                total_parameter_num += parameter_num
+                compressed_parameter_num += (
+                    parameter_num * quantized_data["compression_ratio"]
+                )
+                parameter_numbers.append(parameter_num)
+                quantization_levels.append(0)
+            elif "quantization_level" in quantized_data:
+                parameter_num = numpy.prod(quantized_data["sign_tensor"].shape)
+                total_parameter_num += parameter_num
+                compressed_parameter_num += (
+                    parameter_num * quantized_data["compression_ratio"]
+                )
+                parameter_numbers.append(parameter_num)
+                quantization_levels.append(quantized_data["quantization_level"])
+            else:
+                for v in quantized_data.values():
+                    collection(v)
+
+        collection(quantized_data)
+        assert parameter_numbers
+
+        parameter_ratio = [a / total_parameter_num for a in parameter_numbers]
+        get_logger().info(
+            "avg quantization level %s",
+            sum([a * b for a, b in zip(quantization_levels, parameter_ratio)]),
+        )
+        get_logger().info(
+            "NN compression ratio is %s", compressed_parameter_num / total_parameter_num
+        )
+
+
+class NeuralNetworkImprovedStochasticDequant(ImprovedStochasticDequant):
+    def __call__(self, data: Any) -> Any:
+        match data:
+            case dict():
+                if "dtype" in data:
+                    return super().__call__(data)
+                return {k: self.__call__(v) for k, v in data.items()}
+            case _:
+                return data
+
+
+def NNISQ(
+    weight: float = None,
+    use_l2_norm: bool = False,
+    normalization: bool = True,
+) -> Tuple[Callable, Callable]:
+    return (
+        NeuralNetworkImprovedStochasticQuant(
+            weight=weight,
+            use_l2_norm=use_l2_norm,
+            normalization=normalization,
+        ),
+        NeuralNetworkImprovedStochasticDequant(),
+    )
+
+
+def ISQ(
+    weight: float,
+    use_l2_norm: bool = False,
+    normalization: bool = True,
+) -> Tuple[Callable, Callable]:
+    return (
+        ImprovedStochasticQuant(
+            weight=weight,
+            use_l2_norm=use_l2_norm,
+            normalization=normalization,
+        ),
+        ImprovedStochasticDequant(),
+    )
--- cyy_torch_algorithm/test/quantization/test_schochastic.py
+++ cyy_torch_algorithm/test/quantization/test_schochastic.py
@@ -2,8 +2,14 @@
 import pickle
 
 import torch
+from cyy_naive_lib.algorithm.mapping_op import get_mapping_values_by_key_order
+from cyy_private_torch_algorithm.quantization.stochastic import (
+    ISQ, NNISQ, NeuralNetworkImprovedStochasticQuant)
 from cyy_torch_algorithm.quantization.stochastic import stochastic_quantization
 from cyy_torch_toolbox.default_config import DefaultConfig
+from cyy_torch_toolbox.model_util import ModelUtil
+from cyy_torch_toolbox.tensor import cat_tensors_to_vector
+from transformers import BertModel
 
 
 def test_stochastic_quantization():
@@ -31,3 +37,101 @@ def test_stochastic_quantization():
         "l2 relative diff",
         torch.linalg.norm(recovered_tensor - a) / torch.linalg.norm(a),
     )
+
+
+def test_model_stochastic_quantization():
+    model = BertModel.from_pretrained("bert-base-cased")
+    model_util = ModelUtil(model)
+    model_dict = model_util.get_parameter_dict()
+    model_vect = cat_tensors_to_vector(get_mapping_values_by_key_order(model_dict))
+    print("parameter size is", model_vect.shape)
+
+    quant, dequant = stochastic_quantization(quantization_level=255, use_l2_norm=True)
+    quantization_result = quant(model_vect)
+    quantized_model_vect = dequant(quantization_result)
+    print("old compression ratio", 1 / 4)
+    print(
+        "old relative diff",
+        torch.linalg.norm(quantized_model_vect - model_vect)
+        / torch.linalg.norm(model_vect),
+    )
+    quant, dequant = stochastic_quantization(quantization_level=255, use_l2_norm=False)
+    quantization_result = quant(model_vect)
+    quantized_model_vect = dequant(quantization_result)
+    print("old infinity norm compression ratio", 1 / 4)
+    print(
+        "old infinity norm relative diff",
+        torch.linalg.norm(quantized_model_vect - model_vect)
+        / torch.linalg.norm(model_vect),
+    )
+    weight = 0.01
+
+    quant, dequant = ISQ(weight=weight, use_l2_norm=True, normalization=False)
+    quantization_result = quant(model_vect)
+    quantized_model_vect = dequant(quantization_result)
+    print("use quantization_level ", quantization_result["quantization_level"])
+    print("ISQ L2 norm compression ratio", quantization_result["compression_ratio"])
+    print(
+        "ISQ L2 norm relative diff",
+        torch.linalg.norm(quantized_model_vect - model_vect)
+        / torch.linalg.norm(model_vect),
+    )
+
+    quant, dequant = ISQ(weight=weight, normalization=False)
+    quantization_result = quant(model_vect)
+    quantized_model_vect = dequant(quantization_result)
+    print("use quantization_level ", quantization_result["quantization_level"])
+    print(
+        "infinity norm diff compression ratio", quantization_result["compression_ratio"]
+    )
+    print(
+        "infinity norm diff relative diff",
+        torch.linalg.norm(quantized_model_vect - model_vect)
+        / torch.linalg.norm(model_vect),
+    )
+
+    quant, dequant = ISQ(weight=weight, normalization=True)
+    quantization_result = quant(model_vect)
+    quantized_model_vect = dequant(quantization_result)
+    print("use quantization_level ", quantization_result["quantization_level"])
+    print(
+        "infinity norm diff and normalization compression ratio",
+        quantization_result["compression_ratio"],
+    )
+    print(
+        "infinity norm diff and normalization relative diff",
+        torch.linalg.norm(quantized_model_vect - model_vect)
+        / torch.linalg.norm(model_vect),
+    )
+
+    quant, dequant = NNISQ(weight=weight)
+    quantization_result = quant(model_dict)
+    NeuralNetworkImprovedStochasticQuant.check_compression_ratio(quantization_result)
+    quantized_model = dequant(quantization_result)
+    quantized_model_vect = cat_tensors_to_vector(
+        get_mapping_values_by_key_order(quantized_model)
+    )
+
+    print(
+        "layer by layer relative diff",
+        torch.linalg.norm(quantized_model_vect - model_vect)
+        / torch.linalg.norm(model_vect),
+    )
+
+
+def test_zero_tensor():
+    tensor = torch.zeros(5)
+    quant, dequant = ISQ(weight=0.1)
+    res = quant(tensor)
+    tensor2 = dequant(res)
+    assert torch.all(tensor == tensor2)
+    tensor = torch.zeros(5, dtype=torch.int)
+    quant, dequant = ISQ(weight=0.1)
+    res = quant(tensor)
+    tensor2 = dequant(res)
+    assert torch.all(tensor == tensor2)
+    tensor = torch.zeros(5, dtype=torch.uint8)
+    quant, dequant = ISQ(weight=0.1)
+    res = quant(tensor)
+    tensor2 = dequant(res)
+    assert torch.all(tensor == tensor2)
