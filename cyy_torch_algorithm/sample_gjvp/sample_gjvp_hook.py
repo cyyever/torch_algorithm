f7ce3aea1e19d5daf9aae46405f8080e77911f00
cyy <cyyever@outlook.com>
2022-07-09 10:24:28 +0800

    fix code

--- cyy_torch_algorithm/sample_gjvp/sample_gjvp_hook.py
+++ cyy_torch_algorithm/sample_gjvp/sample_gjvp_hook.py
@@ -1,12 +1,57 @@
 import functools
-from typing import Callable
 
+import torch
+import torch.cuda
 from cyy_torch_algorithm.sample_computation_hook import SampleComputationHook
+from cyy_torch_toolbox.device import put_data_to_device
+from evaluation import eval_model
+from functorch import grad, jvp, vmap
 
-from .sample_jvp import sample_jvp_worker_fun
+
+def sample_gjvp_worker_fun(
+    vector,
+    model_with_loss,
+    sample_indices,
+    inputs,
+    input_features,
+    targets,
+    worker_device,
+    worker_stream,
+):
+    model_with_loss.model.to(worker_device)
+    parameter_list = model_with_loss.model_util.get_parameter_list()
+    with torch.cuda.stream(worker_stream):
+        vector = put_data_to_device(vector, device=worker_device, non_blocking=True)
+        is_input_feature = input_features[0] is not None
+        if is_input_feature:
+            inputs = input_features
+        inputs = put_data_to_device(inputs, device=worker_device, non_blocking=True)
+
+        def jvp_wrapper(parameter_list, input_tensor, target):
+            f = functools.partial(
+                eval_model,
+                targets=target,
+                device=worker_device,
+                model_with_loss=model_with_loss,
+                input_shape=inputs[0].shape,
+                is_input_feature=is_input_feature,
+                non_blocking=True,
+            )
+
+            def grad_f(input_tensor):
+                return grad(f, argnums=0)(parameter_list, input_tensor).view(-1)
+
+            return jvp(grad_f, (input_tensor.view(-1),), (vector,))[1]
+
+        products = vmap(jvp_wrapper, in_dims=(None, 0, 0), randomness="different",)(
+            parameter_list,
+            torch.stack(inputs),
+            torch.stack(targets),
+        )
+        return dict(zip(sample_indices, products))
 
 
-class SampleJVPHook(SampleComputationHook):
+class SampleGradientJVPHook(SampleComputationHook):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.__vector = None
@@ -15,4 +60,4 @@ class SampleJVPHook(SampleComputationHook):
         self.__vector = vector
 
     def _get_worker_fun(self):
-        return functools.partial(sample_jvp_worker_fun, self.__vector)
+        return functools.partial(sample_gjvp_worker_fun, self.__vector)
