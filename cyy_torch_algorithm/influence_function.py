40dc1bb060eac2ace7ffda3526802ee763eae546
cyy <cyyever@outlook.com>
2022-07-19 22:47:43 +0800

    fix code

--- cyy_torch_algorithm/influence_function.py
+++ cyy_torch_algorithm/influence_function.py
@@ -1,47 +1,11 @@
 from cyy_torch_toolbox.ml_type import MachineLearningPhase
 from cyy_torch_toolbox.trainer import Trainer
 
-from cyy_torch_algorithm.sample_gradient.sample_gradient_util import \
-    get_sample_gradient_dict
+from cyy_torch_algorithm.sample_gradient_product.sample_gradient_product_hook import \
+    get_sample_gradient_product_dict
 
-from .inverse_hessian_vector_product import (
-    stochastic_inverse_hessian_vector_product,
-    stochastic_inverse_hessian_vector_product_new)
-
-
-def compute_influence_function(
-    trainer: Trainer,
-    computed_indices,
-    test_gradient=None,
-    dampling_term=0,
-    scale=1,
-    epsilon=0.0001,
-) -> dict:
-
-    if test_gradient is None:
-        inferencer = trainer.get_inferencer(phase=MachineLearningPhase.Test)
-        test_gradient = inferencer.get_gradient()
-
-    product = stochastic_inverse_hessian_vector_product(
-        trainer.dataset,
-        trainer.copy_model_with_loss(True),
-        test_gradient,
-        repeated_num=3,
-        max_iteration=None,
-        batch_size=trainer.hyper_parameter.batch_size,
-        dampling_term=dampling_term,
-        scale=scale,
-        epsilon=epsilon,
-    ) / len(trainer.dataset)
-
-    inferencer = trainer.get_inferencer(phase=MachineLearningPhase.Training)
-    training_sample_gradient_dict = get_sample_gradient_dict(
-        inferencer, computed_indices
-    )
-    return {
-        sample_index: (product @ sample_gradient).data.item()
-        for (sample_index, sample_gradient) in training_sample_gradient_dict.iterate()
-    }
+from .inverse_hessian_vector_product import \
+    stochastic_inverse_hessian_vector_product_new
 
 
 def compute_influence_function_new(
@@ -69,10 +33,4 @@ def compute_influence_function_new(
     ) / len(trainer.dataset)
 
     inferencer = trainer.get_inferencer(phase=MachineLearningPhase.Training)
-    training_sample_gradient_dict = get_sample_gradient_dict(
-        inferencer, computed_indices
-    )
-    return {
-        sample_index: (product @ sample_gradient).data.item()
-        for sample_index, sample_gradient in training_sample_gradient_dict.items()
-    }
+    return get_sample_gradient_product_dict(inferencer, product, computed_indices)
---
+++ cyy_torch_algorithm/sample_gradient_product/sample_gradient_product_hook.py
@@ -0,0 +1,78 @@
+import copy
+import functools
+
+import torch
+from cyy_torch_algorithm.evaluation import eval_model
+from cyy_torch_algorithm.sample_computation_hook import SampleComputationHook
+from cyy_torch_toolbox.device import put_data_to_device
+from cyy_torch_toolbox.ml_type import ModelExecutorHookPoint
+from functorch import grad, vmap
+
+
+def sample_gradient_product_worker_fun(
+    vector,
+    model_with_loss,
+    sample_indices,
+    inputs,
+    input_features,
+    targets,
+    batch_dim,
+    worker_device,
+    worker_stream,
+):
+    vector = put_data_to_device(vector, worker_device)
+    is_input_feature = input_features[0] is not None
+    gradient_lists = vmap(
+        grad(
+            functools.partial(
+                eval_model,
+                device=worker_device,
+                model_with_loss=model_with_loss,
+                is_input_feature=is_input_feature,
+            )
+        ),
+        in_dims=(None, 0, 0),
+        randomness="different",
+    )(
+        model_with_loss.model_util.get_parameter_list(detach=True).to(worker_device),
+        torch.stack(input_features) if is_input_feature else torch.stack(inputs),
+        torch.stack(targets),
+    )
+    return {
+        idx: vector @ gradient for idx, gradient in zip(sample_indices, gradient_lists)
+    }
+
+
+class SampleGradientProductHook(SampleComputationHook):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.__vector = None
+
+    def set_vector(self, vector):
+        self.__vector = vector
+
+    def _get_worker_fun(self):
+        return functools.partial(sample_gradient_product_worker_fun, self.__vector)
+
+
+def get_sample_gradient_product_dict(inferencer, vector, computed_indices=None) -> dict:
+    tmp_inferencer = copy.deepcopy(inferencer)
+    hook = SampleGradientProductHook()
+    if computed_indices is not None:
+        hook.set_computed_indices(computed_indices)
+    hook.set_vector(vector)
+    products: dict = {}
+    tmp_inferencer.append_hook(hook)
+
+    def collect_gradients(**kwargs):
+        nonlocal products
+        products |= hook.sample_result_dict
+
+    tmp_inferencer.append_named_hook(
+        hook_point=ModelExecutorHookPoint.AFTER_BATCH,
+        name="collect_gradients",
+        fun=collect_gradients,
+    )
+    tmp_inferencer.inference(use_grad=True)
+    assert products
+    return products
