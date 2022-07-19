7f9be7d682a405e485ad559c18a314db7402e2e0
cyy <cyyever@outlook.com>
2022-07-19 16:48:23 +0800

    add code

--- cyy_torch_algorithm/inverse_hessian_vector_product.py
+++ cyy_torch_algorithm/inverse_hessian_vector_product.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
+import copy
+
 import torch
 from cyy_naive_lib.log import get_logger
 from cyy_torch_toolbox.device import get_device
+from cyy_torch_toolbox.inferencer import Inferencer
+from cyy_torch_toolbox.ml_type import (ModelExecutorHookPoint,
+                                       StopExecutingException)
 from cyy_torch_toolbox.model_with_loss import ModelWithLoss
+
+from cyy_torch_algorithm.batch_hvp.batch_hvp_hook import BatchHVPHook
 
 from .hessian_vector_product import get_hessian_vector_product_func
 
@@ -38,7 +45,6 @@ def stochastic_inverse_hessian_vector_product(
     product_list = []
     for cur_repeated_id in range(repeated_num):
         cur_product = v
-        diff = None
         iteration = 0
 
         epoch = 1
@@ -70,4 +76,75 @@ def stochastic_inverse_hessian_vector_product(
             get_logger().debug(
                 "stochastic_inverse_hessian_vector_product epoch is %s", epoch
             )
+    return sum(product_list) / len(product_list)
+
+
+def stochastic_inverse_hessian_vector_product_new(
+    inferencer: Inferencer,
+    v,
+    repeated_num=1,
+    max_iteration=1000,
+    dampling_term=0,
+    scale=1,
+    epsilon=0.0001,
+):
+    get_logger().info(
+        "repeated_num is %s,max_iteration is %s,dampling term is %s,scale is %s,epsilon is %s",
+        repeated_num,
+        max_iteration,
+        dampling_term,
+        scale,
+        epsilon,
+    )
+
+    def iteration():
+        cur_product = copy.deepcopy(v)
+        iteration_num = 0
+        hook = BatchHVPHook()
+
+        epoch = 1
+
+        def set_vectors(**kwargs):
+            nonlocal cur_product
+            hook.set_vectors([cur_product])
+
+        tmp_inferencer = copy.deepcopy(inferencer)
+        tmp_inferencer.append_named_hook(
+            hook_point=ModelExecutorHookPoint.AFTER_BATCH,
+            name="set_vectors",
+            fun=set_vectors,
+        )
+        tmp_inferencer.append_hook(hook)
+        result = None
+
+        def compute_product(**kwargs) -> None:
+            nonlocal cur_product
+            nonlocal result
+            nonlocal iteration_num
+            next_product = (
+                v
+                + (1 - dampling_term) * cur_product
+                - hook.sample_result_dict[0][0].to(get_device()) / scale
+            )
+            diff = torch.dist(cur_product, next_product)
+            get_logger().debug(
+                "diff is %s, iteration is %s, max_iteration is %s",
+                diff,
+                iteration_num,
+                max_iteration,
+            )
+            cur_product = next_product
+            iteration_num += 1
+            if (diff <= epsilon or iteration_num >= max_iteration) and epoch > 1:
+                result = cur_product / scale
+                raise StopExecutingException()
+
+        while result is None:
+            tmp_inferencer.inference(use_grad=True, epoch=epoch)
+            epoch += 1
+            get_logger().debug(
+                "stochastic_inverse_hessian_vector_product epoch is %s", epoch
+            )
+
+    product_list = [iteration() for _ in range(repeated_num)]
     return sum(product_list) / len(product_list)
