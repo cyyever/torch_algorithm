79de9fe9dd3f23c5949ea8bc42d351be5e70a263
cyy <cyyever@outlook.com>
2022-08-02 14:53:25 +0800

    add code

--- cyy_torch_algorithm/test/test_sample_gradient.py
+++ cyy_torch_algorithm/test/test_sample_gradient.py
@@ -20,28 +20,6 @@ def test_CV_sample_gradient():
     trainer.append_hook(hook)
 
     def print_sample_gradients(**kwargs):
-        if hook.sample_result_dict:
-            print(hook.sample_result_dict)
-            raise StopExecutingException()
-
-    trainer.append_named_hook(
-        ModelExecutorHookPoint.AFTER_FORWARD, "check gradients", print_sample_gradients
-    )
-    trainer.train()
-
-
-def test_NLP_sample_gradient():
-    config = DefaultConfig("IMDB", "simplelstm")
-    config.hyper_parameter_config.epoch = 1
-    config.hyper_parameter_config.batch_size = 8
-    config.hyper_parameter_config.learning_rate = 0.01
-    config.hyper_parameter_config.find_learning_rate = False
-    trainer = config.create_trainer()
-    hook = SampleGradientHook()
-    hook.set_computed_indices(set(range(10)))
-    trainer.append_hook(hook)
-
-    def print_sample_gradients(**kwargs):
         if hook.result_dict:
             print(hook.result_dict)
             raise StopExecutingException()
