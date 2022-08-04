0402c5329633986c5f9d193b2ff263edf3c9aa59
cyy <cyyever@outlook.com>
2022-08-04 12:19:49 +0800

    add code

--- cyy_torch_algorithm/sample_gradient/sample_gradient_hook.py
+++ cyy_torch_algorithm/sample_gradient/sample_gradient_hook.py
@@ -5,7 +5,6 @@ import torch
 from cyy_torch_algorithm.evaluation import eval_model
 from cyy_torch_algorithm.sample_computation_hook import (SampleComputationHook,
                                                          sample_dot_product)
-from cyy_torch_toolbox.device import put_data_to_device
 from functorch import grad, vmap
 
 
