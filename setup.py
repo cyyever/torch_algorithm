3401e745766b8a168528d72a8b2c706d8d9c75ca
cyy <cyyever@outlook.com>
2022-08-08 14:01:54 +0800

    fix code

--- setup.py
+++ setup.py
@@ -18,10 +18,6 @@ setuptools.setup(
         "cyy_torch_algorithm/computation",
         "cyy_torch_algorithm/data_structure",
         "cyy_torch_algorithm/shapely_value",
-        "cyy_torch_algorithm/sample_gradient",
-        "cyy_torch_algorithm/batch_hvp",
-        "cyy_torch_algorithm/sample_gvjp",
-        "cyy_torch_algorithm/sample_gjvp",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
