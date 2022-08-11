df0272371fdce20ac788939b307106084807e111
cyy <cyyever@outlook.com>
2022-08-11 15:09:46 +0800

    fix code

--- cyy_torch_algorithm/test/computation/test_batch_hvp.py
+++ cyy_torch_algorithm/test/computation/test_batch_hvp.py
@@ -35,9 +35,7 @@ def test_CV_jvp():
             products = hook.result_dict[0]
             assert products.shape[0] == 100
             get_logger().error("use time %s", time_counter.elapsed_milliseconds())
-            assert (
-                torch.linalg.vector_norm(products[0] * 2- products[1]).item() < 0.05
-            )
+            assert torch.linalg.vector_norm(products[0] * 2 - products[1]).item() < 0.05
             raise StopExecutingException()
 
     trainer.append_named_hook(
