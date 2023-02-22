0d86a7c522ac5a084c9fc32b3a3a6f2a833321e6
cyy <cyyever@outlook.com>
2023-02-22 16:22:08 +0800

    disable SyncedTensorDict when no extension is detected

--- cyy_torch_algorithm/data_structure/synced_tensor_dict.py
+++ cyy_torch_algorithm/data_structure/synced_tensor_dict.py
@@ -4,86 +4,92 @@ from typing import Generator
 from cyy_naive_lib.algorithm.sequence_op import split_list_to_chunks
 from cyy_naive_lib.fs.tempdir import get_temp_dir
 from cyy_naive_lib.log import get_logger
-from cyy_torch_cpp_extension.data_structure import SyncedTensorDictIMPL
 
+try:
+    from cyy_torch_cpp_extension.data_structure import SyncedTensorDictIMPL
 
-class SyncedTensorDict(MutableMapping):
-    def __init__(self, tensor_dict, key_type=int):
-        self.__tensor_dict = tensor_dict
-        self.__key_type = key_type
-        self.__iterated_keys = None
-        self.__cache_size = self.__tensor_dict.get_in_memory_number()
-        self.__prefetch_size: int = 0
+    class SyncedTensorDict(MutableMapping):
+        def __init__(self, tensor_dict, key_type=int):
+            self.__tensor_dict = tensor_dict
+            self.__key_type = key_type
+            self.__iterated_keys = None
+            self.__cache_size = self.__tensor_dict.get_in_memory_number()
+            self.__prefetch_size: int = 0
 
-    def __contains__(self, key):
-        return self.__tensor_dict.__contains__(str(key))
+        def __contains__(self, key):
+            return self.__tensor_dict.__contains__(str(key))
 
-    def __getitem__(self, key):
-        return self.__tensor_dict.__getitem__(str(key))
+        def __getitem__(self, key):
+            return self.__tensor_dict.__getitem__(str(key))
 
-    def __setitem__(self, key, value):
-        self.__tensor_dict.__setitem__(str(key), value.detach().cpu())
+        def __setitem__(self, key, value):
+            self.__tensor_dict.__setitem__(str(key), value.detach().cpu())
 
-    def __delitem__(self, key):
-        self.__tensor_dict.__delitem__(str(key))
+        def __delitem__(self, key):
+            self.__tensor_dict.__delitem__(str(key))
 
-    def __len__(self):
-        return len(self.__tensor_dict)
+        def __len__(self):
+            return len(self.__tensor_dict)
 
-    def __iter__(self):
-        self.__iterated_keys = self.__keys()
-        self.__prefetch_size = self.__cache_size
-        return self
-
-    def __next__(self):
-        if not self.__iterated_keys:
-            raise StopIteration()
-        key = self.__iterated_keys[0]
-        self.__iterated_keys = self.__iterated_keys[1:]
-        self.__prefetch_size -= 1
-        if self.__prefetch_size <= 0:
+        def __iter__(self):
+            self.__iterated_keys = self.__keys()
             self.__prefetch_size = self.__cache_size
-            self.prefetch(self.__iterated_keys[: self.__prefetch_size])
-        return key
+            return self
 
-    def __keys(self) -> list:
-        return [self.__eval_key(k) for k in self.__tensor_dict.keys()]
+        def __next__(self):
+            if not self.__iterated_keys:
+                raise StopIteration()
+            key = self.__iterated_keys[0]
+            self.__iterated_keys = self.__iterated_keys[1:]
+            self.__prefetch_size -= 1
+            if self.__prefetch_size <= 0:
+                self.__prefetch_size = self.__cache_size
+                self.prefetch(self.__iterated_keys[: self.__prefetch_size])
+            return key
 
-    def __eval_key(self, k):
-        if self.__key_type is None:
-            return eval(k)
-        return self.__key_type(k)
+        def __keys(self) -> list:
+            return [self.__eval_key(k) for k in self.__tensor_dict.keys()]
 
-    def prefetch(self, keys: list) -> None:
-        self.__tensor_dict.prefetch([str(k) for k in keys])
+        def __eval_key(self, k):
+            if self.__key_type is None:
+                return eval(k)
+            return self.__key_type(k)
 
-    def __getattr__(self, name):
-        return getattr(self.__tensor_dict, name)
+        def prefetch(self, keys: list) -> None:
+            self.__tensor_dict.prefetch([str(k) for k in keys])
 
-    def iterate(self, keys: set | list | None = None) -> Generator:
-        if keys is None:
-            keys = list(self.__tensor_dict.keys())
-        else:
-            keys = list(set(str(k) for k in keys))
-        for chunk in split_list_to_chunks(keys, self.__cache_size // 2):
-            self.__tensor_dict.prefetch(chunk)
-            for k in chunk:
-                yield (self.__eval_key(k), self.__tensor_dict[k])
+        def __getattr__(self, name):
+            return getattr(self.__tensor_dict, name)
 
-    @classmethod
-    def create(
-        cls,
-        storage_dir=None,
-        key_type=int,
-        cache_size=None,
-    ):
-        if storage_dir is None:
-            storage_dir = get_temp_dir().name
-            impl = SyncedTensorDictIMPL(storage_dir)
-        else:
-            impl = SyncedTensorDictIMPL(storage_dir)
-            impl.set_permanent_storage()
-        if cache_size is not None:
-            impl.set_in_memory_number(cache_size)
-        get_logger().info("tensor_dict use cache size %s", impl.get_in_memory_number())
-        return cls(tensor_dict=impl, key_type=key_type)
+        def iterate(self, keys: set | list | None = None) -> Generator:
+            if keys is None:
+                keys = list(self.__tensor_dict.keys())
+            else:
+                keys = list(set(str(k) for k in keys))
+            for chunk in split_list_to_chunks(keys, self.__cache_size // 2):
+                self.__tensor_dict.prefetch(chunk)
+                for k in chunk:
+                    yield (self.__eval_key(k), self.__tensor_dict[k])
+
+        @classmethod
+        def create(
+            cls,
+            storage_dir=None,
+            key_type=int,
+            cache_size=None,
+        ):
+            if storage_dir is None:
+                storage_dir = get_temp_dir().name
+                impl = SyncedTensorDictIMPL(storage_dir)
+            else:
+                impl = SyncedTensorDictIMPL(storage_dir)
+                impl.set_permanent_storage()
+            if cache_size is not None:
+                impl.set_in_memory_number(cache_size)
+            get_logger().info(
+                "tensor_dict use cache size %s", impl.get_in_memory_number()
+            )
+            return cls(tensor_dict=impl, key_type=key_type)
+
+except BaseException:
+    pass
--- cyy_torch_algorithm/test/data_structure/test_synced_tensor_dict.py
+++ cyy_torch_algorithm/test/data_structure/test_synced_tensor_dict.py
@@ -1,16 +1,20 @@
 import torch
-from cyy_torch_algorithm.data_structure.synced_tensor_dict import \
-    SyncedTensorDict
 
-tensor_dict = SyncedTensorDict.create(cache_size=10)
+try:
+    from cyy_torch_algorithm.data_structure.synced_tensor_dict import \
+        SyncedTensorDict
 
-for i in range(100):
-    tensor_dict[i] = torch.tensor([i])
+    tensor_dict = SyncedTensorDict.create(cache_size=10)
 
-for (key, tensor) in tensor_dict.items():
-    assert tensor == torch.tensor([key])
+    for i in range(100):
+        tensor_dict[i] = torch.tensor([i])
 
-for (key, tensor) in tensor_dict.iterate({"1", "2"}):
-    assert 1 <= key <= 2
-    assert tensor == torch.tensor([key])
-tensor_dict.flush()
+    for (key, tensor) in tensor_dict.items():
+        assert tensor == torch.tensor([key])
+
+    for (key, tensor) in tensor_dict.iterate({"1", "2"}):
+        assert 1 <= key <= 2
+        assert tensor == torch.tensor([key])
+    tensor_dict.flush()
+except BaseException:
+    pass
