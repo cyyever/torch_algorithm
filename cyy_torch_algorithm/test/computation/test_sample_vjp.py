1476338931b1297768a9c2e2d56bc54dd384cfef
cyy <cyyever@outlook.com>
2023-11-26 22:45:17 +0800

    Fix code

--- cyy_torch_algorithm/test/computation/test_sample_vjp.py
+++ cyy_torch_algorithm/test/computation/test_sample_vjp.py
@@ -30,19 +30,17 @@ def test_CV_vjp():
 
 
 def test_NLP_vjp():
-    return
     config = Config("IMDB", "TransformerClassificationModel")
     config.dc_config.dataset_kwargs["max_len"] = 300
+    config.dc_config.dataset_kwargs["tokenizer"] = {"type": "spacy"}
     config.model_config.model_kwargs["max_len"] = 300
     config.model_config.model_kwargs["d_model"] = 100
     config.model_config.model_kwargs["nhead"] = 5
     config.model_config.model_kwargs["num_encoder_layer"] = 1
     config.hyper_parameter_config.epoch = 1
     config.hyper_parameter_config.learning_rate = 0.1
-    config.hyper_parameter_config.find_learning_rate = False
     trainer = config.create_trainer()
     trainer.model_util.freeze_modules(module_type=nn.Embedding)
-    trainer.model_evaluator.need_input_features = True
     hook = SampleGradientVJPHook()
     hook.set_vector(torch.ones_like(trainer.model_util.get_parameter_list()).view(-1))
     trainer.append_hook(hook)
