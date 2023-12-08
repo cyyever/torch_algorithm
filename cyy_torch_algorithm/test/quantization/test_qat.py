78c3ba51716a0e855168885ff414cf27a364a2f2
cyy <cyyever@outlook.com>
2023-12-08 10:38:58 +0800

    Fix code

--- cyy_torch_algorithm/test/quantization/test_qat.py
+++ cyy_torch_algorithm/test/quantization/test_qat.py
@@ -1,8 +1,14 @@
+import importlib.util
+
 from cyy_torch_algorithm.quantization.qat import QuantizationAwareTraining
 from cyy_torch_toolbox.default_config import Config
 
+has_cyy_torch_vision: bool = importlib.util.find_spec("cyy_torch_vision") is not None
+
 
 def test_training() -> None:
+    if not has_cyy_torch_vision:
+        return
     trainer = Config("MNIST", "Lenet5").create_trainer()
     trainer.hyper_parameter.epoch = 1
     trainer.hyper_parameter.learning_rate = 0.01
