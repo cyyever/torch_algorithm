6dca5efa3b7400d4d5a0843a2fd8a18d8d6581b0
cyy <cyyever@outlook.com>
2024-03-13 10:28:54 +0800

    Add test

--- cyy_torch_algorithm/test/computation/test_sample_vjp.py
+++ cyy_torch_algorithm/test/computation/test_sample_vjp.py
@@ -65,3 +65,35 @@ def test_NLP_vjp() -> None:
         ExecutorHookPoint.AFTER_BATCH, "check gradients", print_result
     )
     trainer.train()
+
+
+def test_hugging_face_vjp() -> None:
+    if not has_cyy_torch_text:
+        return
+    import cyy_torch_text  # noqa: F401
+
+    config = Config(
+        "imdb", "hugging_face_sequence_classification_distilbert-base-cased"
+    )
+    config.dc_config.dataset_kwargs["input_max_len"] = 300
+    config.model_config.model_kwargs["pretrained"] = True
+    config.model_config.model_kwargs["n_layers"] = 1
+    config.model_config.model_kwargs["frozen_modules"] = ["distilbert.embeddings"]
+    config.hyper_parameter_config.epoch = 1
+    config.hyper_parameter_config.learning_rate = 0.1
+    trainer = config.create_trainer()
+    trainer.model_util.freeze_modules(module_type=nn.Embedding)
+    hook = SampleGradientVJPHook()
+    hook.set_vector(torch.ones_like(trainer.model_util.get_parameter_list()).view(-1))
+    trainer.append_hook(hook)
+
+    def print_result(**kwargs):
+        if hook.result_dict:
+            print(hook.result_dict)
+            hook.reset()
+            raise StopExecutingException()
+
+    trainer.append_named_hook(
+        ExecutorHookPoint.AFTER_BATCH, "check gradients", print_result
+    )
+    trainer.train()
