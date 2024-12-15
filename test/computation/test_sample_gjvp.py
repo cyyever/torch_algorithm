dc30505542ae45f8cd051ab061433aab585612b9
cyy <cyyever@outlook.com>
2024-12-15 17:33:22 +0800

    Fix code

--- test/computation/test_sample_gjvp.py
+++ test/computation/test_sample_gjvp.py
@@ -35,33 +35,3 @@ def test_CV_jvp() -> None:
         ExecutorHookPoint.AFTER_BATCH, "check results", print_products
     )
     trainer.train()
-
-
-def test_NLP_vjp() -> None:
-    if not has_cyy_torch_text:
-        return
-    import cyy_torch_text  # noqa: F401
-
-    config = Config("imdb", "TransformerClassificationModel")
-    config.dc_config.dataset_kwargs["input_max_len"] = 300
-    config.dc_config.dataset_kwargs["tokenizer"] = {"type": "spacy"}
-    config.model_config.model_kwargs["d_model"] = 100
-    config.model_config.model_kwargs["nhead"] = 5
-    config.model_config.model_kwargs["num_encoder_layer"] = 1
-    config.hyper_parameter_config.epoch = 1
-    config.hyper_parameter_config.learning_rate = 0.1
-    trainer = config.create_trainer()
-    hook = SampleGradientJVPHook()
-    hook.set_vector(torch.ones((1, 100 * 300)).view(-1))
-    trainer.append_hook(hook)
-
-    def print_result(**kwargs):
-        if hook.result_dict:
-            print(hook.result_dict)
-            hook.reset()
-            raise StopExecutingException()
-
-    trainer.append_named_hook(
-        ExecutorHookPoint.AFTER_BATCH, "check results", print_result
-    )
-    trainer.train()
